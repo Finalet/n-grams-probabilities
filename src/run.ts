a997703acf4ad1daeb33b41e6ac6ee7952b8cbd1
Grant Oganyan <grant1599@mail.ru>
2024-01-05 22:35:23 -0500

    Support for any N-gram. Added russian

--- src/run.ts
+++ src/run.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 
 type CharDictionary = Map<string, Map<string, number>>;
 
-const inputFileName = "english-20000.txt";
+const inputFileName = "russian-50000.txt";
 const probabilityAccuracy = 3;
 const accuracy = Math.pow(10, probabilityAccuracy);
 const marginOfError = 10 / accuracy;
@@ -13,67 +13,77 @@ async function Run() {
   const input = fs.readFileSync(__dirname + `/../input/${inputFileName}`, "utf8");
   const words = input.split("\n");
 
-  const dictionary: CharDictionary = new Map<string, Map<string, number>>();
+  for (let n = 1; n <= 5; n++) {
+    const counts = getCharacterCounts(words, n);
+    const probabilities = getProbabilitiesFromCounts(counts);
 
-  for (const word of words) {
-    GetCharacterCounts(word, dictionary);
+    await SaveOutput(counts, `-n${n}-counts`);
+    await SaveOutput(probabilities, `-n${n}-probabilities`);
   }
-  await SaveOutput(dictionary, "-counts");
-
-  ConvertCountsToProbabilities(dictionary);
-  VerifyProbabilities(dictionary);
-
-  await SaveOutput(dictionary, "-probabilities");
 
   console.log("\n\n--- End ---");
 }
 
-function GetCharacterCounts(word: string, dictionary: CharDictionary) {
-  const cleanWord = word.toLowerCase().trim();
-  if (!cleanWord.match(/^[a-z]+$/)) return;
+function getCharacterCounts(words: string[], n: number = 2): CharDictionary {
+  const dictionary: CharDictionary = new Map<string, Map<string, number>>();
 
-  const characters = cleanWord.split("");
-  for (let i = 0; i < characters.length; i++) {
-    if (i === characters.length - 1) break;
-
-    const char = characters[i];
-    const nextChar = characters[i + 1];
-
-    if (!dictionary.has(char)) {
-      dictionary.set(char, new Map<string, number>());
-    }
-
-    dictionary.get(char)?.set(nextChar, (dictionary.get(char)?.get(nextChar) || 0) + 1);
+  for (const word of words) {
+    SetCharacterCountsForWord(word, n, dictionary);
   }
 
-  dictionary.forEach((dict, char) => {
+  dictionary.forEach((dict, str) => {
     const sorted = new Map([...dict.entries()].sort((a, b) => b[1] - a[1]));
-    dictionary.set(char, sorted);
+    dictionary.set(str, sorted);
   });
   const sorted = new Map([...dictionary.entries()].sort());
-  dictionary.clear();
-  sorted.forEach((dict, char) => {
-    dictionary.set(char, dict);
-  });
+  return sorted;
 }
 
-function ConvertCountsToProbabilities(dictionary: CharDictionary) {
-  dictionary.forEach((dict, char) => {
-    const total = [...dict.values()].reduce((acc, next) => acc + next, 0);
-    dict.forEach((count, secChar) => {
-      const probability = Math.round((accuracy * count) / total) / accuracy;
-      dict.set(secChar, probability);
+function SetCharacterCountsForWord(word: string, n: number, dictionary: CharDictionary) {
+  const cleanWord = word.toLowerCase().trim().replace(/ё/g, "е");
+  if (!cleanWord.match(/^[a-z]+$/) && !cleanWord.match(/^[абвгдеёжзийклмнопрстуфхцчшщъыьэюя]+$/)) return;
+  if (n >= cleanWord.length) return;
+
+  const steps = cleanWord.length - n;
+  for (let i = 0; i < steps; i++) {
+    const subStr = cleanWord.substring(i, i + n);
+    const nextChar = cleanWord[i + n];
+
+    if (!dictionary.has(subStr)) {
+      dictionary.set(subStr, new Map<string, number>());
+    }
+
+    const currentCount = dictionary.get(subStr)?.get(nextChar) || 0;
+    dictionary.get(subStr)?.set(nextChar, currentCount + 1);
+  }
+}
+
+function getProbabilitiesFromCounts(dictionary: CharDictionary): CharDictionary {
+  const probabilities: CharDictionary = new Map<string, Map<string, number>>();
+
+  dictionary.forEach((dict, str) => {
+    probabilities.set(str, new Map<string, number>());
+    dict.forEach((count, char) => {
+      probabilities.get(str)?.set(char, count);
     });
   });
-}
 
-function VerifyProbabilities(dictionary: CharDictionary) {
-  dictionary.forEach((dict, char) => {
+  probabilities.forEach((dict, str) => {
+    const total = [...dict.values()].reduce((acc, next) => acc + next, 0);
+    dict.forEach((count, char) => {
+      const probability = Math.round((accuracy * count) / total) / accuracy;
+      dict.set(char, probability);
+    });
+  });
+
+  probabilities.forEach((dict, str) => {
     const total = Math.round(accuracy * [...dict.values()].reduce((acc, next) => acc + next, 0)) / accuracy;
     if (total < 1 - marginOfError || total > 1 + marginOfError) {
-      console.log(`[error:${char}] Total probability is beyond margin of error: ${total}`);
+      console.log(`[error:${str}] Total probability is beyond margin of error: ${total}`);
     }
   });
+
+  return probabilities;
 }
 
 async function SaveOutput(dictionary: CharDictionary, suffix = "") {
